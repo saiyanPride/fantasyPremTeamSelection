76f58d7b96c15c7f58933d413ebbd754591a062a
saiyanPride <none@example.com>
2018-01-13 14:58:23 +0000

    [unit test update]: substitutes and starters merge rule
    verifies substitutes and starters are merged to give a list where
    players are sorted in descending order of their estimated score for
    next week

--- engine/tests/TeamTest.cpp
+++ engine/tests/TeamTest.cpp
@@ -1,22 +1,37 @@
 #include "catch.hpp"
 #include "Team.hpp"
+#include <algorithm>
 using namespace fantasypremierleague;
-Team myTeam;// impliclty updates the team (populates starting lineup & substitutes from datasource)
+Team myTeam; // impliclty updates the team (populates starting lineup & substitutes from datasource)
 
-TEST_CASE("whenCurrentTeamIsRetrievedStartingLineUpHas11Players"){
-    REQUIRE(myTeam.getStartingLineUp().size()==11);
+TEST_CASE("whenCurrentTeamIsRetrievedStartingLineUpHas11Players")
+{
+    REQUIRE(myTeam.getStartingLineUp().size() == 11);
 };
 
-TEST_CASE("checkThatThereAre4Substitues"){
-    REQUIRE(myTeam.getSubstitutes().size()==4);
+TEST_CASE("checkThatThereAre4Substitues")
+{
+    REQUIRE(myTeam.getSubstitutes().size() == 4);
 }
 
-TEST_CASE("whenTeamIsUpdatedTwiceStartingLineupHas11Players"){
+TEST_CASE("whenTeamIsUpdatedTwiceStartingLineupHas11Players")
+{
     myTeam.updateTeam();
-    REQUIRE(myTeam.getStartingLineUp().size()==11);
+    REQUIRE(myTeam.getStartingLineUp().size() == 11);
 }
 
-TEST_CASE("whenTeamIsUpdatedTwiceSubstitutesListHas4Players"){
+TEST_CASE("whenTeamIsUpdatedTwiceSubstitutesListHas4Players")
+{
     myTeam.updateTeam();
-    REQUIRE(myTeam.getSubstitutes().size()==4);
+    REQUIRE(myTeam.getSubstitutes().size() == 4);
+}
+
+TEST_CASE("whenStartingLineUpandSubstitutesAreMergedPlayersAreSortedByAverageScore")
+{
+    const auto &mergedTeamList = myTeam.getMergedTeamList();
+    auto sortPredicate = [](const Player &a, const Player &b) {
+        return a.getAvgFutureScore() > b.getAvgFutureScore();
+    };
+    bool isMergedListInRightOrder = std::is_sorted(mergedTeamList.begin(), mergedTeamList.end(), sortPredicate);
+    REQUIRE(isMergedListInRightOrder);
 }
